9c3677db20bbe946326021afd13747e774a05ece
TonyBhaskar <tonybhaskar23@gmail.com>
2024-10-14 17:04:10 +0530

    For loop Example

--- src/com/basics/oct14/ForLoopExample.java
+++ src/com/basics/oct14/ForLoopExample.java
@@ -3,8 +3,16 @@ package com.basics.oct14;
 public class ForLoopExample {
 
 	public static void main(String[] args) {
+		
+		// Printing 1 to 10 using for loop 
 		for(int i=1; i<=10; i++) {
 			System.out.println(i);
+		}
+		
+		//Looping through an array 
+		int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(i+":"+arr[i]);
 		}
 		
 	}

b8b4bc71fc07694ccfa92645faa1099c3ecb5ae3
TonyBhaskar <tonybhaskar23@gmail.com>
2024-10-16 14:42:48 +0530

    commit

--- src/scanner/oct16/OperatorsCode.java
+++ src/scanner/oct16/OperatorsCode.java
@@ -80,7 +80,27 @@ class Operators{
 		
 	}
 	
-	
+	void relationalOperators() {
+		System.out.println("Relational Operators: ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		int age = sc.nextInt();
+		if(age < 16)
+			System.out.println("School kid");
+		else if(age >=16)
+			System.out.println("Completed Schooling");
+		System.out.println("--------------------------");
+		if(age <= 22)
+			System.out.println("college kid");
+		else if(age > 22)
+			System.out.println("Completed college");
+		System.out.println("--------------------------");
+		if(age == 27)
+			System.out.println("Right age for the marriage");
+		else if(age != 45)
+			System.out.println("might be married ");
+		System.out.println("--------------------------");
+	}
 }
 
 public class OperatorsCode {
@@ -88,6 +108,7 @@ public class OperatorsCode {
 	public static void main(String[] args) {
 		Operators ob = new Operators();
 		ob.arithmeticOperators();
+		ob.relationalOperators();
 	}
 
 }

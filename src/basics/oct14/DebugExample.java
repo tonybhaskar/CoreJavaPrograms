5a2f9b1424dc6c0515b71485aa678cda6fdc8181
TonyBhaskar <tonybhaskar23@gmail.com>
2024-10-19 18:33:55 +0530

    commit

---
+++ src/basics/oct14/DebugExample.java
@@ -0,0 +1,12 @@
+package basics.oct14;
+
+public class DebugExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=10; i++) {
+			int number = i;
+			System.out.println(number);
+		}
+	}
+
+}

c625d4a08ccfd88cd5059638878644b73eb61b05
TonyBhaskar <tonybhaskar23@gmail.com>
2024-10-22 17:50:23 +0530

    alphabets all

---
+++ src/patternProgramming4/oct22/AlphabetB.java
@@ -0,0 +1,35 @@
+package patternProgramming4.oct22;
+
+import java.util.Scanner;
+
+public class AlphabetB {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size: ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i<n; i++) {
+			
+			for(int j = 0; j<n; j++) {
+				if(i==0 && j!=0 && j!=(n-1) || j==0 && i!=0 || j==(n-1)&& i!=0 || i==(n/2)) {
+					System.out.print("# ");
+				}else {
+					System.out.print("  ");
+				}
+			}
+			System.out.print("  ");
+			
+			for(int j = 0; j<n; j++) {
+				if( ((i==0 || i==(n-1) || (i==(n/2))) && j!=(n-1)) || (j==(n-1) && i!=0 && i!=(n-1) && i!=(n/2)) || j==0) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
